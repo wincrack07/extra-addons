954dbdbf83ce4fdd19b6832d0b19a662c268a38b
Julio Saldaña <102325762+wincrack07@users.noreply.github.com>
2023-08-30 09:34:08 -0500

    Delete pw_pos_salesperson 2 directory

--- pw_pos_salesperson 2/models/pos_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-
-class PosOrderLine(models.Model):
-    _inherit = 'pos.order.line'
-
-    user_id = fields.Many2one('res.users', string='Salesperson')
